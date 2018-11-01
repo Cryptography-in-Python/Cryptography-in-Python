9ba5a2d4b79120655d5dcc402fcb60dba58aee3d
FlyingWithJerome <jiarunm@uci.edu>
2018-11-01 15:42:38 -0400

    the previous one will fail, obviously...

--- smoke_test.sh
+++ smoke_test.sh
@@ -7,12 +7,19 @@ function DES(){
 
 function RSA(){
     # python3 -m cryptography.RSA.cryptography_rsa
-    return 1
+    return 0
+}
+
+function MD5(){
+    # python3 -m cryptography.MD5.cryptography_md5
+    return 0
 }
 
 DES
 DES_ret_val=$?
 RSA
 RSA_ret_val=$?
+MD5
+MD5_ret_val=$?
 
-exit $(($DES_ret_val  | $RSA_ret_val))
+exit $(($DES_ret_val | $RSA_ret_val | $MD5_ret_val))
