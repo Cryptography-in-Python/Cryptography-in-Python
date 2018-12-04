2b7f895345cdd3f2c1bbd539305ae8d16cf2be89
Tan Li <txl555@case.edu>
2018-12-04 16:08:50 -0500

    update

--- cryptography/AES/aes.py
+++ cryptography/AES/aes.py
@@ -6,9 +6,9 @@ class AES(object):
     '''AES funtions for a single block
     '''
     # valid key sizes
-    keySize = dict(SIZE_128=16, SIZE_192=24, SIZE_256=32)
+    keySize = dict(SIZE_128=16)
 
-    # Rijndael S-box
+    #S-box
     sbox =  [0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67,
             0x2b, 0xfe, 0xd7, 0xab, 0x76, 0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59,
             0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0, 0xb7,
@@ -34,7 +34,7 @@ class AES(object):
             0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0,
             0x54, 0xbb, 0x16]
 
-    # Rijndael Inverted S-box
+    #Inverted S-box
     rsbox = [0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3,
             0x9e, 0x81, 0xf3, 0xd7, 0xfb , 0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f,
             0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb , 0x54,
@@ -61,22 +61,15 @@ class AES(object):
             0x21, 0x0c, 0x7d]
 
     def getSBoxValue(self,num):
-        """Retrieves a given S-Box Value"""
         return self.sbox[num]
 
     def getSBoxInvert(self,num):
-        """Retrieves a given Inverted S-Box Value"""
         return self.rsbox[num]
 
     def rotate(self, word):
-        """ Rijndael's key schedule rotate operation.
-
-        Rotate a word eight bits to the left: eg, rotate(1d2c3a4f) == 2c3a4f1d
-        Word is an char list of size 4 (32 bits overall).
-        """
         return word[1:] + word[:1]
 
-    # Rijndael Rcon
+    #Rcon
     Rcon = [0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36,
             0x6c, 0xd8, 0xab, 0x4d, 0x9a, 0x2f, 0x5e, 0xbc, 0x63, 0xc6, 0x97,
             0x35, 0x6a, 0xd4, 0xb3, 0x7d, 0xfa, 0xef, 0xc5, 0x91, 0x39, 0x72,
@@ -103,11 +96,9 @@ class AES(object):
             0xe8, 0xcb ]
 
     def getRconValue(self, num):
-        """Retrieves a given Rcon Value"""
         return self.Rcon[num]
 
     def core(self, word, iteration):
-        """Key schedule core."""
         # rotate the 32-bit word 8 bits to the left
         word = self.rotate(word)
         # apply S-Box substitution on all 4 parts of the 32-bit word
@@ -119,13 +110,6 @@ class AES(object):
         return word
 
     def expandKey(self, key, size, expandedKeySize):
-        """Rijndael's key expansion.
-
-        Expands an 128,192,256 key into an 176,208,240 bytes key
-
-        expandedKey is a char list of large enough size,
-        key is the non-expanded key.
-        """
         # current expanded keySize, in bytes
         currentSize = 0
         rconIteration = 1
@@ -148,10 +132,6 @@ class AES(object):
             # For 256-bit keys, we add an extra sbox to the calculation
             if size == self.keySize["SIZE_256"] and ((currentSize % size) == 16):
                 for l in range(4): t[l] = self.getSBoxValue(t[l])
-
-            # We XOR t with the four-byte block 16,24,32 bytes before the new
-            # expanded key.  This becomes the next four bytes in the expanded
-            # key.
             for m in range(4):
                 expandedKey[currentSize] = expandedKey[currentSize - size] ^ \
                         t[m]
@@ -160,16 +140,11 @@ class AES(object):
         return expandedKey
 
     def addRoundKey(self, state, roundKey):
-        """Adds (XORs) the round key to the state."""
         for i in range(16):
             state[i] ^= roundKey[i]
         return state
 
     def createRoundKey(self, expandedKey, roundKeyPointer):
-        """Create a round key.
-        Creates a round key from the given expanded key and the
-        position within the expanded key.
-        """
         roundKey = [0] * 16
         for i in range(4):
             for j in range(4):
@@ -177,7 +152,6 @@ class AES(object):
         return roundKey
 
     def galois_multiplication(self, a, b):
-        """Galois multiplication of 8 bit characters a and b."""
         p = 0
         for counter in range(8):
             if b & 1: p ^= a
@@ -190,10 +164,6 @@ class AES(object):
             b >>= 1
         return p
 
-    #
-    # substitute all the values from the state with the value in the SBox
-    # using the state value as index for the SBox
-    #
     def subBytes(self, state, isInv):
         if isInv: getter = self.getSBoxInvert
         else: getter = self.getSBoxValue
@@ -304,20 +274,9 @@ class AES(object):
         block = [0] * 16
         # set the number of rounds
         if size == self.keySize["SIZE_128"]: nbrRounds = 10
-        elif size == self.keySize["SIZE_192"]: nbrRounds = 12
-        elif size == self.keySize["SIZE_256"]: nbrRounds = 14
-        else: return None
 
         # the expanded keySize
         expandedKeySize = 16*(nbrRounds+1)
-
-        # Set the block values, for the block:
-        # a0,0 a0,1 a0,2 a0,3
-        # a1,0 a1,1 a1,2 a1,3
-        # a2,0 a2,1 a2,2 a2,3
-        # a3,0 a3,1 a3,2 a3,3
-        # the mapping order is a0,0 a1,0 a2,0 a3,0 a0,1 a1,1 ... a2,3 a3,3
-        #
         # iterate over the columns
         for i in range(4):
             # iterate over the rows
@@ -354,13 +313,6 @@ class AES(object):
         # the expanded keySize
         expandedKeySize = 16*(nbrRounds+1)
 
-        # Set the block values, for the block:
-        # a0,0 a0,1 a0,2 a0,3
-        # a1,0 a1,1 a1,2 a1,3
-        # a2,0 a2,1 a2,2 a2,3
-        # a3,0 a3,1 a3,2 a3,3
-        # the mapping order is a0,0 a1,0 a2,0 a3,0 a0,1 a1,1 ... a2,3 a3,3
-
         # iterate over the columns
         for i in range(4):
             # iterate over the rows
@@ -379,11 +331,6 @@ class AES(object):
 
 
 class AESModeOfOperation(object):
-    '''Handles AES with plaintext consistingof multiple blocks.
-    Choice of block encoding modes:  OFB, CFB, CBC
-    '''
-    # Very annoying code:  all is for an object, but no state is kept!
-    # Should just be plain functions in an AES_BlockMode module.
     aes = AES()
 
     # structure of supported modes of operation
@@ -422,13 +369,6 @@ class AESModeOfOperation(object):
             j += 1
             i += 1
         return ar
-
-    # Mode of Operation Encryption
-    # stringIn - Input String
-    # mode - mode of type modeOfOperation
-    # hexKey - a hex key of the bit length size
-    # size - the bit length of the key
-    # hexIV - the 128 bit hex Initilization Vector
     def encrypt(self, stringIn, mode, key, size, IV):
         if len(key) % size:
             return None
@@ -500,13 +440,6 @@ class AESModeOfOperation(object):
                         cipherOut.append(ciphertext[k])
         return mode, len(stringIn), cipherOut
 
-    # Mode of Operation Decryption
-    # cipherIn - Encrypted String
-    # originalsize - The unencrypted string length - required for CBC
-    # mode - mode of type modeOfOperation
-    # key - a number array of the bit length size
-    # size - the bit length of the key
-    # IV - the 128 bit number array Initilization Vector
     def decrypt(self, cipherIn, originalsize, mode, key, size, IV):
         # cipherIn = unescCtrlChars(cipherIn)
         if len(key) % size:
@@ -581,91 +514,10 @@ class AESModeOfOperation(object):
                             chrOut.append(chr(plaintext[k]))
                     iput = ciphertext
         return "".join(chrOut)
-
-
-def append_PKCS7_padding(s):
-    """return s padded to a multiple of 16-bytes by PKCS7 padding"""
-    numpads = 16 - (len(s)%16)
-    return s + numpads*chr(numpads)
-
-def strip_PKCS7_padding(s):
-    """return s stripped of PKCS7 padding"""
-    if len(s)%16 or not s:
-        raise ValueError("String of len %d can't be PCKS7-padded" % len(s))
-    numpads = ord(s[-1])
-    if numpads > 16:
-        raise ValueError("String ending with %r can't be PCKS7-padded" % s[-1])
-    return s[:-numpads]
-
-def encryptData(key, data, mode=AESModeOfOperation.modeOfOperation["CBC"]):
-    """encrypt `data` using `key`
-
-    `key` should be a string of bytes.
-
-    returned cipher is a string of bytes prepended with the initialization
-    vector.
-
-    """
-    key = map(ord, key)
-    if mode == AESModeOfOperation.modeOfOperation["CBC"]:
-        data = append_PKCS7_padding(data)
-    keysize = len(key)
-    assert keysize in AES.keySize.values(), 'invalid key size: %s' % keysize
-    # create a new iv using random data
-    iv = [ord(i) for i in os.urandom(16)]
-    moo = AESModeOfOperation()
-    (mode, length, ciph) = moo.encrypt(data, mode, key, keysize, iv)
-    # With padding, the original length does not need to be known. It's a bad
-    # idea to store the original message length.
-    # prepend the iv.
-    return ''.join(map(chr, iv)) + ''.join(map(chr, ciph))
-
-def decryptData(key, data, mode=AESModeOfOperation.modeOfOperation["CBC"]):
-    """decrypt `data` using `key`
-
-    `key` should be a string of bytes.
-
-    `data` should have the initialization vector prepended as a string of
-    ordinal values.
-    """
-
-    key = map(ord, key)
-    keysize = len(key)
-    assert keysize in AES.keySize.values(), 'invalid key size: %s' % keysize
-    # iv is first 16 bytes
-    iv = map(ord, data[:16])
-    data = map(ord, data[16:])
-    moo = AESModeOfOperation()
-    decr = moo.decrypt(data, None, mode, key, keysize, iv)
-    if mode == AESModeOfOperation.modeOfOperation["CBC"]:
-        decr = strip_PKCS7_padding(decr)
-    return decr
-
-def generateRandomKey(keysize):
-    """Generates a key from random data of length `keysize`.    
-    The returned key is a string of bytes.    
-    """
-    if keysize not in (16, 24, 32):
-        emsg = 'Invalid keysize, %s. Should be one of (16, 24, 32).'
-        raise ValueError(emsg % keysize)
-    return os.urandom(keysize)
-
-def testStr(cleartext, keysize=16, modeName = "CBC"):
-    '''Test with random key, choice of mode.'''
-    print('Random key test', 'Mode:', modeName)
-    print('cleartext:', cleartext)
-    key =  generateRandomKey(keysize)
-    print('Key:', [ord(x) for x in key])
-    mode = AESModeOfOperation.modeOfOperation[modeName]
-    cipher = encryptData(key, cleartext, mode)
-    print('Cipher:', [ord(x) for x in cipher])
-    decr = decryptData(key, cipher, mode)
-    print('Decrypted:', decr)
-    
-    
+ 
 if __name__ == "__main__":
     moo = AESModeOfOperation()
-    cleartext = "hjdfhdoij jf jiojf ioj"
+    cleartext = "hjdfhdoij jjj jf jiojf ioj"
     cypherkey = [143,194,34,208,145,203,230,143,177,246,97,206,145,92,255,84]
     iv = [103,35,148,239,76,213,47,118,255,222,123,176,106,134,98,92]
     mode, orig_len, ciph = moo.encrypt(cleartext, moo.modeOfOperation["CBC"],
